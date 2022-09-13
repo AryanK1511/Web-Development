cc2f1f9f134e73ac1e34f0d706b359b207d8cfe0
Aryan Khurana <aryankhurana1511@gmail.com>
2022-09-13 08:46:27 +0530

    Added Calculator files

---
+++ 005-Backend Development/Calculator/calculator.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+
+const port = 3000;
+
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/index.html');
+})
+
+app.post('/', function(req, res) {
+    let num1 = Number(req.body.num1);
+    let num2 = Number(req.body.num2);
+    let result = num1 + num2;
+    res.send("The result of the calculation is " + result);
+})
+
+app.listen(port, function() {
+    console.log(`Server started at port ${port}`);
+})
