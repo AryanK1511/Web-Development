af5b094311d9ba5f5c7c5cd1f79beff61c9c36e3
Aryan Khurana <aryankhurana1511@gmail.com>
2022-09-21 05:32:12 +0530

    Added JS Folder

---
+++ 004-JavaScript/01-Introduction_to_JS/variables.js
@@ -0,0 +1,21 @@
+var myName = "Aryan";
+console.log(myName); // Aryan
+myName = "Aryan Khurana";
+console.log(myName); // Aryan Khurana
+console.log("My name is " + myName);
+
+// Swapping two numbers
+function test() {
+    var a = "3";
+    var b = "8";
+    var temp = a;
+    a = b;
+    b = temp;
+    console.log("a is " + a);
+    console.log("b is " + b);
+}
+
+
+// String Concatenation
+var s = "Hello " + "World";
+console.log(s);
