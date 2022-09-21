8f292b202fcef9a5c7e48fcbe322f4b436f3f670
Aryan Khurana <aryankhurana1511@gmail.com>
2022-09-21 21:36:52 +0530

    Added whosBuyingLunch challenge file

---
+++ 004-JavaScript/02-Intermediate_JS/arrays.js
@@ -0,0 +1,19 @@
+let guestList = ["A", "B", "C", "D", "E", "F"];
+
+// Accessing elements at index positions
+console.log(guestList[0]);
+
+// Checking whether an array includes an element
+console.log(guestList.includes("A"));
+
+// Pushing and Popping
+let arr = [];
+arr.push(1);
+arr.push(2);
+arr.push(3);
+arr.push(4);
+arr.push(5);
+console.log(arr);
+arr.pop();
+arr.pop();
+console.log(arr);
---
+++ 004-JavaScript/02-Intermediate_JS/whosBuyingLunch.js
@@ -0,0 +1,11 @@
+function whosPaying(names) {
+    
+    /******Don't change the code above*******/
+        
+        //Write your code here.
+        let possibleIndex = Math.floor((Math.random()) * names.length);
+        let value = `${names[possibleIndex]} is going to buy lunch today!`;
+        return value;
+    
+    /******Don't change the code below*******/    
+}
