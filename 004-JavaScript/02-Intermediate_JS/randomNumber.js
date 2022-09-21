c9abe347be043fa6fbb98722a9f83431ce2ffed1
Aryan Khurana <aryankhurana1511@gmail.com>
2022-09-21 17:09:03 +0530

    Added randomNumber file

---
+++ 004-JavaScript/02-Intermediate_JS/randomNumber.js
@@ -0,0 +1,8 @@
+var n = Math.random();
+
+// Generating a number between 1 and 6 inclusive
+// Method 1
+console.log(Math.ceil(n * 6));
+
+// Method 2
+console.log(Math.floor(n * 6) + 1);
---
+++ Challenge 01 - Dice Game/index.js
@@ -0,0 +1,2 @@
+var n = Math.random();
+console.log(Math.ceil(n * 6));
