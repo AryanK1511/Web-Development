14e788b0c2d13a73bb62997e3f43c1fb2c12a4e1
Aryan Khurana <aryankhurana1511@gmail.com>
2022-09-21 22:54:01 +0530

    Added JS Files

---
+++ 004-JavaScript/02-Intermediate_JS/loops.js
@@ -0,0 +1,13 @@
+// While loops
+console.log("WHILE LOOP:");
+let count = 1;
+while (count <= 5) {
+    console.log(count);
+    count++;
+}
+
+// For Loop
+console.log("\nFOR LOOP:");
+for (let i = 1; i <= 5; i++) {
+    console.log(i);
+}
