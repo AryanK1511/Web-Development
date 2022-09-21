222a868ba98d446c546fda94e6d25fe3cf0f0275
Aryan Khurana <aryankhurana1511@gmail.com>
2022-09-21 05:50:16 +0530

    Added arithmetic.js

---
+++ 004-JavaScript/01-Introduction_to_JS/arithmetic.js
@@ -0,0 +1,20 @@
+console.log(6/4); // 1.5
+console.log(6%4); // 2
+console.log(12%8); // 2
+
+// Check whether a number is odd or even
+var num = 5;
+if (num % 2 == 0) {
+    console.log(num + " is an even number.");
+}
+else {
+    console.log(num + " is an odd number.");
+}
+
+// Order of precedence in operators
+// PEDMAS - Parenthesis, exponents, division, multiplication, addition, subtraction
+
+// Increment and Decrement
+var x = 5;
+x++; // x = x + 1 OR x += 1
+console.log(x); 
