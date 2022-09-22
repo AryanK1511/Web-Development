a8bcf58b10251b89b5c5724cc03616fd4275866f
Aryan Khurana <aryankhurana1511@gmail.com>
2022-09-22 18:51:01 +0530

    Made some changes

--- 005-Backend Development/WeatherProject/app.js
+++ 005-Backend Development/WeatherProject/app.js
@@ -5,9 +5,17 @@ const app = express();
 app.get("/", function(req, res) {
     const url = "https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=17bfc9a8e13796f5eb3fff46ede4db9e";
     https.get(url, function(response) {
-        console.log(response);
+        // console.log(response);
+        response.on("data", function(data) {
+            const weatherData = JSON.parse(data);
+            // Opposite of parse: JSON.stringify(object);
+            const temp = weatherData.main.temp;
+            const weatherDescription = weatherData.weather[0].description;
+            response.write("<h2>" + weatherDescription + "</h2>")
+            response.write("<h1>Temperature in London is " + temp + "degree Kelvin.</h1>");
+            response.send();
+        })
     });
-    res.send("Server is up and running.");
 });
 
 app.listen(3000, function() {
