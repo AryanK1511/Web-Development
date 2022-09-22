45d06bec711e0f06d7a2c9a410e76520193e8fc3
Aryan Khurana <aryankhurana1511@gmail.com>
2022-09-22 11:34:44 +0530

    Added drum kit website completed code

--- 004-JavaScript/04-AdvancedJS_and_DOM/Drum_Kit_Website/index.js
+++ 004-JavaScript/04-AdvancedJS_and_DOM/Drum_Kit_Website/index.js
@@ -13,32 +13,49 @@ let tom4Sound = new Audio('./sounds/tom-4.mp3');
 for (let i = 0; i < numberOfDrumButtons; i++) {
     document.querySelectorAll(".drum")[i].addEventListener('click', function() {
         var buttonInnerHTML = this.innerHTML;
-
-        // Using switch case to determine which button was clicked
-        switch (buttonInnerHTML) {
-            case 'w':
-                crashSound.play();
-                break;
-            case 'a':
-                kickBassSound.play();
-                break;
-            case 's':
-                snareSound.play();
-                break;
-            case 'd':
-                tom1Sound.play();
-                break;
-            case 'j':
-                tom2Sound.play();
-                break;
-            case 'k':
-                tom3Sound.play();
-                break;
-            case 'l':
-                tom4Sound.play();
-                break;
-            default:
-                console.log(buttonInnerHTML);
-        }
+        makeSound(buttonInnerHTML);
+        buttonAnimation(buttonInnerHTML);
     });    
+}
+
+document.addEventListener('keydown', function(e) {
+    makeSound(e.key);
+    buttonAnimation(e.key);
+});
+
+function makeSound(key) {
+    // Using switch case to determine which button was clicked
+    switch (key) {
+        case 'w':
+            crashSound.play();
+            break;
+        case 'a':
+            kickBassSound.play();
+            break;
+        case 's':
+            snareSound.play();
+            break;
+        case 'd':
+            tom1Sound.play();
+            break;
+        case 'j':
+            tom2Sound.play();
+            break;
+        case 'k':
+            tom3Sound.play();
+            break;
+        case 'l':
+            tom4Sound.play();
+            break;
+        default:
+            console.log(buttonInnerHTML);
+    }
+}
+
+function buttonAnimation(currentKey) {
+    var activeButton = document.querySelector('.' + currentKey);
+    activeButton.classList.add('pressed');
+    setTimeout(function() {
+        activeButton.classList.remove('pressed');
+    }, 100)
 }
