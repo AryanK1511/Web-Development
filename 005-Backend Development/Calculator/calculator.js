fb5e131c2f8a31a07c8ad2513c71c4fef3550628
Aryan Khurana <aryankhurana1511@gmail.com>
2022-09-23 18:46:05 +0530

    Changed calculator app files

--- 005-Backend Development/Calculator/calculator.js
+++ 005-Backend Development/Calculator/calculator.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
+// urlencoded is used when you want to get data from a form
 app.use(bodyParser.urlencoded({extended: true}));
 
 const port = 3000;
