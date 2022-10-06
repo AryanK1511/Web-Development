4d1f81d7d40f38541acfd524fb269290c0bcc296
Aryan Khurana <aryankhurana1511@gmail.com>
2022-10-06 09:18:51 -0400

    Set up success and failure pages

--- 005-Backend Development/APIs - Application Programming Interfaces/Newsletter-Signup/app.js
+++ 005-Backend Development/APIs - Application Programming Interfaces/Newsletter-Signup/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
+const https = require("https");
 
 const app = express();
 
@@ -17,9 +18,47 @@ app.post("/", function(req, res) {
     const lastname = req.body.lastname;
     const email = req.body.email;
 
-    console.log(firstname + lastname + email);
+    var data = {
+        members: [
+            {
+                email_address: email,
+                status: "subscribed",
+                merge_fields: {
+                    FNAME: firstname,
+                    LNAME: lastname
+                }
+            }
+        ]
+    }
+
+    const jsonData = JSON.stringify(data);
+
+    const url = "https://us14.api.mailchimp.com/3.0/lists/3b92e106f5";
+
+    const options = {
+        method: "POST",
+        auth: "AryanK1511:e2c49bbacf85a49168770e3d88287a2d-us14"
+    }
+
+    const request = https.request(url, options, function(response) {
+        if (response.statusCode === 200) {
+            res.sendFile(__dirname + "/success.html");
+        }
+        else {
+            res.sendFile(__dirname + "/failure.html");
+        }
+        response.on("data", function(data){
+            console.log(JSON.parse(data));
+        })
+    })
+
+    request.write(jsonData);
+    request.end();
 });
 
 app.listen(3000, () => {
     console.log("Server is running on port 3000.");
 })
+
+// API Key : e2c49bbacf85a49168770e3d88287a2d-us14
+// List ID: 3b92e106f5
