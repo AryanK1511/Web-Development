b1f804dc859453001bea99172058830e85b3e79c
Aryan Khurana <aryankhurana1511@gmail.com>
2022-10-06 07:39:32 -0400

    Added complete weatherProject code

--- 005-Backend Development/APIs - Application Programming Interfaces/WeatherProject/app.js
+++ 005-Backend Development/APIs - Application Programming Interfaces/WeatherProject/app.js
@@ -1,9 +1,20 @@
 const express = require("express");
 const https = require("https");
+const bodyParser = require('body-parser');
+
 const app = express();
 
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.get("/", function(req, res) {
-    const url = "https://api.openweathermap.org/data/2.5/weather?q=London&appid=17bfc9a8e13796f5eb3fff46ede4db9e&units=metric";
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.post("/", function(req, res) {
+    const query = req.body.cityName;
+    const apiKey = "17bfc9a8e13796f5eb3fff46ede4db9e";
+    const unit = "metric";
+    const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&appid=" + apiKey + "&units=" + unit;
 
     // Using the https module
     https.get(url, function(response) {
@@ -19,13 +30,13 @@ app.get("/", function(req, res) {
             
             // res.write() doesnt display html without this line
             res.set("Content-Type", "text/html");
-            res.write("<p>The weather is currently " + weatherDescription + "</p>");
-            res.write("<h1>The temperature in London is " + temp + " degrees Celcius.</h1>");
+            res.write("<p>" + weatherDescription + "</p>");
+            res.write("<h1>The temperature in " + query + " is " + temp + " degrees Celcius.</h1>");
             res.write("<img src='" + imageURL + "'>");
             res.end();
         })
     })
-});
+})
 
 app.listen(3000, function() {
     console.log("Server is running on port 3000.");
