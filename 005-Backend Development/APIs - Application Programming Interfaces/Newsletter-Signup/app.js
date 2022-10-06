66f82dbac212d3e749905d6eab938cb577142950
Aryan Khurana <aryankhurana1511@gmail.com>
2022-10-06 09:32:20 -0400

    Added code for newsletter web app

--- 005-Backend Development/APIs - Application Programming Interfaces/Newsletter-Signup/app.js
+++ 005-Backend Development/APIs - Application Programming Interfaces/Newsletter-Signup/app.js
@@ -56,6 +56,10 @@ app.post("/", function(req, res) {
     request.end();
 });
 
+app.post("/failure", function(req, res) {
+    res.redirect("/");
+})
+
 app.listen(3000, () => {
     console.log("Server is running on port 3000.");
 })
