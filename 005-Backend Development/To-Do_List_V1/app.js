41e0742b4eb457e496920ce229ae9f66a26c6ef9
Aryan Khurana <aryankhurana1511@gmail.com>
2022-10-07 07:34:15 -0400

    Added basic EJS code

--- 005-Backend Development/To-Do_List_V1/app.js
+++ 005-Backend Development/To-Do_List_V1/app.js
@@ -3,8 +3,22 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+app.set('view engine', 'ejs');
+
 app.get("/", function(req, res) {
-    res.send("Hello");
+
+   var today = new Date();
+   var currentDay = today.getDay();
+   var day = "";
+
+   if (currentDay === 6 || currentDay === 0) {
+    day = "Weekend";
+    res.render('list', {kindOfDay: day});
+   }
+   else {
+    day = "Weekday";
+    res.render('list', {kindOfDay: day});
+   }
 });
 
 app.listen(3000, function() {
