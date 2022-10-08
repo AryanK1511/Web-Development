bf9407961dffa88d7fec2f4f51f8b98febdc46af
Aryan Khurana <aryankhurana1511@gmail.com>
2022-10-08 08:42:24 -0400

    Completed challenge 18 and 19

--- Challenge 02 - Blog Website_V1/app.js
+++ Challenge 02 - Blog Website_V1/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const _ = require("lodash");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 
@@ -22,12 +23,20 @@ app.get("/", (req, res) => {
 })
 
 app.get("/posts/:postTitle", (req, res) => {
-  const requestedTitle = req.params.postTitle;
+  const requestedTitle = _.lowerCase(req.params.postTitle);
+
   posts.forEach(function(post) {
-    const storedTitle = post.postTitle;
+    const storedTitle = _.lowerCase(post.postTitle);
 
     if (storedTitle === requestedTitle) {
-      console.log("Match Found");
+      for (let i = 0; i < posts.length; i++) {
+        if (_.lowerCase(posts[i].postTitle) === requestedTitle) {
+          res.render("post", {requestedTitle: posts[i].postTitle, postBody: posts[i].postBody});
+        }
+      }
+    }
+    else {
+      console.log("Match Not Found");
     }
   })
 })
