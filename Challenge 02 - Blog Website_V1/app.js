82b0b676fba7234189bdf3a4f7271988918cf5cf
Aryan Khurana <aryankhurana1511@gmail.com>
2022-10-08 07:16:56 -0400

    Completed challenge 5

--- Challenge 02 - Blog Website_V1/app.js
+++ Challenge 02 - Blog Website_V1/app.js
@@ -19,6 +19,14 @@ app.get("/", (req, res) => {
   res.render("home", {homeDisplayContent: homeStartingContent});
 })
 
+app.get("/about", (req, res) => {
+  res.render("about", {aboutDisplayContent: aboutContent});
+})
+
+app.get("/contact", (req, res) => {
+  res.render("contact", {contactDisplayContent: contactContent});
+})
+
 app.listen(3000, function() {
   console.log("Server started on port 3000");
 });
