a67956f2673b69056867c480bbb8263e730318f3
Aryan Khurana <aryankhurana1511@gmail.com>
2022-10-11 20:52:10 -0400

    Introduction to mongoose code addition

--- 006-Databases/MongoDB/FruitsProject/app.js
+++ 006-Databases/MongoDB/FruitsProject/app.js
@@ -9,10 +9,47 @@ const fruitSchema = new mongoose.Schema ({
 
 const Fruit = mongoose.model("Fruit", fruitSchema);
 
-const fruit = new Fruit ({
-    name: "Apple",
-    rating: 7,
-    review: "Pretty solid as a fruit."
+// To Insert a single Fruit
+// const fruit = new Fruit ({
+//     name: "Apple",
+//     rating: 7,
+//     review: "Pretty solid as a fruit."
+// })
+
+// fruit.save();
+
+const kiwi = new Fruit({
+    name: "Kiwi",
+    score: 10,
+    review: "The best fruit."
 })
 
-fruit.save();
+const orange = new Fruit({
+    name: "Orange",
+    score: 4,
+    review: "Too sour for me"
+})
+
+const banana = new Fruit({
+    name: "Banana",
+    score: 3,
+    review: "Weird Texture"
+})
+
+Fruit.insertMany([kiwi, orange, banana], function(err) {
+    if (err) {
+        console.log(err);
+    }
+    else {
+        console.log("Successfully saved all fruits to fruitsDB!");
+    }
+});
+
+Fruit.find(function(err, fruits) {
+    if (err) {
+        console.log(err);
+    }
+    else {
+        console.log(fruits);
+    }
+})
