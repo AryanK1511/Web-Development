3dac58842672d9efbc3e0e7d260d5817458d3a93
Aryan Khurana <aryankhurana1511@gmail.com>
2022-10-15 06:03:21 -0400

    Adding a To-Do List item

--- 006-Databases/To-Do_List_V2/app.js
+++ 006-Databases/To-Do_List_V2/app.js
@@ -66,15 +66,16 @@ app.get("/", function(req, res) {
 
 app.post("/", function(req, res){
 
-  const item = req.body.newItem;
+  const itemName = req.body.newItem;
 
-  if (req.body.list === "Work") {
-    workItems.push(item);
-    res.redirect("/work");
-  } else {
-    items.push(item);
-    res.redirect("/");
-  }
+  const item = new Item({
+    name: itemName
+  })
+
+  item.save();
+
+  res.redirect("/");
+
 });
 
 app.get("/work", function(req,res){
