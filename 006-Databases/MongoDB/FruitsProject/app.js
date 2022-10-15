097e159cede95b6bb10d83346f9b72602e542e64
Aryan Khurana <aryankhurana1511@gmail.com>
2022-10-14 21:24:16 -0400

    Added final mongoose code

--- 006-Databases/MongoDB/FruitsProject/app.js
+++ 006-Databases/MongoDB/FruitsProject/app.js
@@ -24,71 +24,97 @@ const fruit = new Fruit ({
     review: "Pretty solid as a fruit."
 })
 
+// To save the changes in our database
 fruit.save();
 
-// const kiwi = new Fruit({
-//     name: "Kiwi",
-//     score: 10,
-//     review: "The best fruit."
-// })
+const kiwi = new Fruit({
+    name: "Kiwi",
+    score: 10,
+    review: "The best fruit."
+})
 
-// const orange = new Fruit({
-//     name: "Orange",
-//     score: 4,
-//     review: "Too sour for me"
-// })
+const orange = new Fruit({
+    name: "Orange",
+    score: 4,
+    review: "Too sour for me"
+})
 
-// const banana = new Fruit({
-//     name: "Banana",
-//     score: 3,
-//     review: "Weird Texture"
-// })
+const banana = new Fruit({
+    name: "Banana",
+    score: 3,
+    review: "Weird Texture"
+})
 
 // Inserting multiple items in your database
-// Fruit.insertMany([kiwi, orange, banana], function(err) {
-//     if (err) {
-//         console.log(err);
-//     }
-//     else {
-//         console.log("Successfully saved all fruits to fruitsDB!");
-//     }
-// });
+Fruit.insertMany([kiwi, orange, banana], function(err) {
+    if (err) {
+        console.log(err);
+    }
+    else {
+        console.log("Successfully saved all fruits to fruitsDB!");
+    }
+});
+
+const personSchema = new mongoose.Schema ({
+    name: String,
+    age: Number,
+    favouriteFruit: fruitSchema
+})
+
+
+const pineapple = new Fruit({
+    name: "Pineapple",
+    score: 9,
+    review: "Great Fruit!"
+})
+
+pineapple.save();
+
+const Person = mongoose.model("Person", personSchema);
+
+const person = new Person({
+    name: "Amy",
+    age: 12,
+    favouriteFruit: pineapple
+})
+
+person.save();
 
 // Reading from database
-// Fruit.find(function(err, fruits) {
-//     if (err) {
-//         console.log(err);
-//     }
-//     else {
-//         mongoose.connection.close();
-//         // Looping through the fruits array and printing only the names
-//         fruits.forEach((fruit) => {
-//             console.log(fruit.name);
-//         })
-//     }
-// })
+Fruit.find(function(err, fruits) {
+    if (err) {
+        console.log(err);
+    }
+    else {
+        mongoose.connection.close();
+        // Looping through the fruits array and printing only the names
+        fruits.forEach((fruit) => {
+            console.log(fruit.name);
+        })
+    }
+})
 
-// Updating data in database
-// Fruit.updateOne({_id: "63476b2c5adceb32fb9c8fbf"}, {name: "Peach", review: "Peaches are pretty good!"}, function(err) {
-//     if (err) {
-//         console.log(err);
-//     }
-//     else {
-//         console.log("Successfully updated the document!");
-//     }
-// })
+// // Updating data in database
+Fruit.updateOne({_id: "63476b2c5adceb32fb9c8fbf"}, {name: "Peach", review: "Peaches are pretty good!"}, function(err) {
+    if (err) {
+        console.log(err);
+    }
+    else {
+        console.log("Successfully updated the document!");
+    }
+})
 
-// Deleting data in a database
-// Fruit.deleteOne({_id: "63476bbfd76ceab628f848a2"}, function(err) {
-//     if (err) {
-//         console.log(err);
-//     }
-//     else {
-//         console.log("Document Successfully deleted");
-//     }
-// })
+// // Deleting data in a database
+Fruit.deleteOne({_id: "63476bbfd76ceab628f848a2"}, function(err) {
+    if (err) {
+        console.log(err);
+    }
+    else {
+        console.log("Document Successfully deleted");
+    }
+})
 
-// You can also use the deleteMany Function
+// // You can also use the deleteMany Function
 Fruit.deleteMany({name: "Apple"}, function(err) {
     if (err) {
         console.log(err);
