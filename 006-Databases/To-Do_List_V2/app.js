c0c9d42f0d246babcc9d491f4068adbef2d5fcae
Aryan Khurana <aryankhurana1511@gmail.com>
2022-10-15 05:29:53 -0400

    Set up mongoose in app

--- 006-Databases/To-Do_List_V2/app.js
+++ 006-Databases/To-Do_List_V2/app.js
@@ -1,8 +1,7 @@
-//jshint esversion:6
-
 const express = require("express");
 const bodyParser = require("body-parser");
 const date = require(__dirname + "/date.js");
+const mongoose = require("mongoose");
 
 const app = express();
 
@@ -11,14 +10,35 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-const items = ["Buy Food", "Cook Food", "Eat Food"];
-const workItems = [];
+// Connecting to the to do list database
+mongoose.connect("mongodb://localhost:27017/todolistDB", { useNewUrlParser: true });
+
+// Creating a schema
+const itemsSchema = new mongoose.Schema({
+  name: String
+})
+
+// Creating a model
+const Item = mongoose.model("Item", itemsSchema);
+
+// Creating default documents
+const item1 = new Item({
+  name: "Welcome to your todolist!"
+})
+
+const item2 = new Item({
+  name: "Hit the + button to add a new item."
+})
+
+const item3 = new Item({
+  name: "<-- Hit this to delete an item."
+})
 
 app.get("/", function(req, res) {
 
 const day = date.getDate();
 
-  res.render("list", {listTitle: day, newListItems: items});
+  res.render("list", {listTitle: "Today", newListItems: items});
 
 });
 
