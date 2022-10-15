82d0cede896d6751db52e09c3b140cb0e8b5d8c5
Aryan Khurana <aryankhurana1511@gmail.com>
2022-10-15 16:11:44 -0400

    Added chained routes

--- 007-Building_RESTful_APIs/Wiki-API/app.js
+++ 007-Building_RESTful_APIs/Wiki-API/app.js
@@ -4,53 +4,61 @@ const ejs = require("ejs");
 const mongoose = require("mongoose");
 
 const app = express();
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 // Connecting to database
-mongoose.connect("mongodb://localhost:27017/wikiDB", {useNewUrlParser: true});
+mongoose.connect("mongodb://localhost:27017/wikiDB", { useNewUrlParser: true });
 
 const articleSchema = {
-    title: String,
-    content: String
-}
+  title: String,
+  content: String,
+};
 
 const Article = mongoose.model("Article", articleSchema);
 
-// When clients want to fetch all the articles in our database
-app.get("/articles", function(req, res) {
-    Article.find({}, function(err, foundArticles) {
-        if (err) {
-            res.send(err);
-        }
-        else {
-            res.send(foundArticles);
-        }
-    })
-})
+// Using chainable routes
+app.route("/articles")
+  // When clients want to fetch all the articles in our database
+  .get(function (req, res) {
+    Article.find({}, function (err, foundArticles) {
+      if (err) {
+        res.send(err);
+      } else {
+        res.send(foundArticles);
+      }
+    });
+  })
 
-// Client creates one new article
-app.post("/articles", function(req, res) {
-    console.log(req.body.title);
-    console.log(req.body.content);
-
+  // Client creates one new article
+  .post(function (req, res) {
     const newArticle = new Article({
-        title: req.body.title,
-        content: req.body.content
+      title: req.body.title,
+      content: req.body.content,
     });
 
-    newArticle.save(function(err) {
-        if (err) {
-            res.send(err);
-        }
-        else {
-            res.send("Successfully added a new article.");
-        }
+    newArticle.save(function (err) {
+      if (err) {
+        res.send(err);
+      } else {
+        res.send("Successfully added a new article.");
+      }
     });
-})
+  })
 
-app.listen(3000,function() {
-    console.log("Server started at port 3000");
-})
+  // Delete all the articles in our collection
+  .delete(function (req, res) {
+    Article.deleteMany(function (err) {
+      if (!err) {
+        res.send("Successfully deleted all articles.");
+      } else {
+        res.send(err);
+      }
+    });
+  });
+
+app.listen(3000, function () {
+  console.log("Server started at port 3000");
+});
