45d6207bf69ff81bce31c33a210455419f506ee9
Aryan Khurana <aryankhurana1511@gmail.com>
2022-10-16 17:38:20 -0400

    Using passport.js and adding cookies and sessions

--- 008-Authentication_and_Security/Secrets/app.js
+++ 008-Authentication_and_Security/Secrets/app.js
@@ -4,9 +4,12 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
 
-const bcrypt = require("bcrypt");
-const saltRounds = 10;
+// const bcrypt = require("bcrypt");
+// const saltRounds = 10;
 
 // const md5 = require("md5");
 // const encrypt = require("mongoose-encryption");
@@ -18,6 +21,20 @@ app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 
+// Place this code here - IMPORTANT
+app.use(session({
+    secret: "Our little secret.",
+    resave: false,
+    saveUninitialized: false
+  }))
+
+// This sets up passport for us to start using it for authentication
+app.use(passport.initialize());
+
+// Use passport for dealing with the sessions
+// Cookies get deleted whenever you restart the server
+app.use(passport.session());
+
 mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true})
 
 const userSchema = new mongoose.Schema({
@@ -25,11 +42,18 @@ const userSchema = new mongoose.Schema({
     password: String
 });
 
+userSchema.plugin(passportLocalMongoose);
+
+const User = new mongoose.model("User", userSchema);
+
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 // Encrypting the database
 // const secret = "Thisisourlittlesecret.";
 // userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ["password"]});
-
-const User = new mongoose.model("User", userSchema);
 
 // ==================== HOME ROUTE ====================
 app.get("/", function(req, res) {
@@ -46,46 +70,96 @@ app.get("/register", function(req, res) {
     res.render("Register");
 })
 
-// ==================== POSTING TO THE REGISTER ROUTE ====================
-app.post("/register", function(req, res) {
-
-    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
-        const newUser = new User({
-            email: req.body.username,
-            password: hash
-        });
-    
-        newUser.save(function(err) {
-            if (err) {
-                console.log(err);
-            }
-            else {
-                res.render("secrets");
-            }
-        })
-    });
+// ==================== SECRETS ROUTE ====================
+app.get("/secrets", function(req, res) {
+    if (req.isAuthenticated()) {
+        res.render("secrets");
+    }
+    else {
+        res.redirect("/login");
+    }
 })
 
-// ==================== POSTING TO THE LOGIN ROUTE ====================
-app.post("/login", function(req, res) {
-    const username = req.body.username;
-    const password = req.body.password;
-
-    User.findOne({email: username}, function(err, foundUser) {
+// ====================== LOGOUT ROUTE ===================
+app.get("/logout", function(req, res) {
+    req.logout(function(err) {
         if (err) {
             console.log(err);
         }
         else {
-            if (foundUser) {
-                // Comparing the two passwords
-                if (bcrypt.compare(password, foundUser.password, function(err, result) {
-                    if (result === true) {
-                        res.render("secrets");
-                    }
-                }));
-            }
+            res.redirect("/");
         }
     });
+})
+
+// ==================== POSTING TO THE REGISTER ROUTE ====================
+app.post("/register", function(req, res) {
+    // bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+    //     const newUser = new User({
+    //         email: req.body.username,
+    //         password: hash
+    //     });
+    
+    //     newUser.save(function(err) {
+    //         if (err) {
+    //             console.log(err);
+    //         }
+    //         else {
+    //             res.render("secrets");
+    //         }
+    //     })
+    // });
+
+    User.register({username: req.body.username}, req.body.password, function(err, user) {
+        if (err) {
+            console.log(err);
+            res.redirect("/register");
+        }
+        else {
+            passport.authenticate("local")(req, res, function() {
+                res.redirect("/secrets");
+            })
+        }
+    })
+})
+
+// ==================== POSTING TO THE LOGIN ROUTE ====================
+app.post("/login", function(req, res) {
+
+    const user = new User({
+        username: req.body.username,
+        password: req.body.password
+    })
+
+    req.login(user, function(err) {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            passport.authenticate("local")(req, res, function() {
+                res.redirect("/secrets");
+            })
+        }
+    })
+
+    // const username = req.body.username;
+    // const password = req.body.password;
+
+    // User.findOne({email: username}, function(err, foundUser) {
+    //     if (err) {
+    //         console.log(err);
+    //     }
+    //     else {
+    //         if (foundUser) {
+    //             // Comparing the two passwords
+    //             if (bcrypt.compare(password, foundUser.password, function(err, result) {
+    //                 if (result === true) {
+    //                     res.render("secrets");
+    //                 }
+    //             }));
+    //         }
+    //     }
+    // });
 });
 
 // Listing for user requests
