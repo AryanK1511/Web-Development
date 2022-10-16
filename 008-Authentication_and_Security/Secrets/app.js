3578aa20474907f73e5391f4bb1c744086d6f565
Aryan Khurana <aryankhurana1511@gmail.com>
2022-10-15 21:08:53 -0400

    Added Database Encryption

--- 008-Authentication_and_Security/Secrets/app.js
+++ 008-Authentication_and_Security/Secrets/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
 
 const app = express();
 
@@ -12,10 +13,14 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true})
 
-const userSchema = {
+const userSchema = new mongoose.Schema({
     email: String,
     password: String
-}
+});
+
+// Encrypting the database
+const secret = "Thisisourlittlesecret.";
+userSchema.plugin(encrypt, {secret: secret, encryptedFields: ["password"]});
 
 const User = new mongoose.model("User", userSchema);
 
