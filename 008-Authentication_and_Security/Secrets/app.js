aa688ba2eaf3e2d2dd725605c9e6cce86b70700c
Aryan Khurana <aryankhurana1511@gmail.com>
2022-10-15 21:32:28 -0400

    Added env variables

--- 008-Authentication_and_Security/Secrets/app.js
+++ 008-Authentication_and_Security/Secrets/app.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
@@ -20,7 +22,7 @@ const userSchema = new mongoose.Schema({
 
 // Encrypting the database
 const secret = "Thisisourlittlesecret.";
-userSchema.plugin(encrypt, {secret: secret, encryptedFields: ["password"]});
+userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ["password"]});
 
 const User = new mongoose.model("User", userSchema);
 
