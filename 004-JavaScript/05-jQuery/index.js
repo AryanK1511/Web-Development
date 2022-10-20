4891a2e2aaddb928efe478cae92e0da22a071db2
Aryan Khurana <aryankhurana1511@gmail.com>
2022-10-19 22:07:13 -0400

    Manipulating attributes with jQuery

--- 004-JavaScript/05-jQuery/index.js
+++ 004-JavaScript/05-jQuery/index.js
@@ -24,3 +24,7 @@ $("h1").text("Bye!");
 
 // Editing innerHtml
 $("button").html("<em>Hey</em>")
+
+// Manipulating attributes with jQuery
+console.log($("img").attr("src"));
+console.log($("a").attr("href", "https://www.youtube.com/"));
