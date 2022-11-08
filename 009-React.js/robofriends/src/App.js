315ee821ce0d2fa8579ba7e5f3b83b356937eecf
Aryan Khurana <aryankhurana1511@gmail.com>
2022-11-08 10:12:07 -0500

    Added dynamic search functionality

--- 009-React.js/robofriends/src/App.js
+++ 009-React.js/robofriends/src/App.js
@@ -3,14 +3,36 @@ import {robots} from './robots'
 import CardList from './CardList'
 import SearchBox from './SearchBox'
 
-const App = () => {
-    return (
-        <div className='tc'>
-            <h1>RoboFriends</h1>
-            <SearchBox />
-            <CardList robots={robots} />
-        </div>
-    )
+const state = {
+    robots: robots,
+    searchField: ''
+}
+
+class App extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            robots: robots,
+            searchField: ''
+        }
+    }
+
+    onSearchChange = (event) => {
+        this.setState({ searchField: event.target.value });
+    }
+
+    render() {
+        const filteredRobots = this.state.robots.filter(robot => {
+            return robot.name.toLowerCase().includes(this.state.searchField.toLowerCase());
+        })
+        return (
+            <div className='tc'>
+                <h1>RoboFriends</h1>
+                <SearchBox searchChange={this.onSearchChange}/>
+                <CardList robots={filteredRobots} />
+            </div>
+        )
+    }
 }
 
 export default App;
--- 009-React.js/robofriends/src/SearchBox.js
+++ 009-React.js/robofriends/src/SearchBox.js
@@ -1,11 +1,12 @@
 import React from 'react'
 
-const SearchBox = () => {
+const SearchBox = ({searchChange}) => {
     return (
         <input 
             className='pa3 ba b--green bg-lightest-blue'
             type='search'
             placeholder='search robots'
+            onChange={searchChange}
         />
     )
 }
