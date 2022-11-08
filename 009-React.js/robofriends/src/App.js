9116e6572ad6c25ceafce3e8c692f801996815bc
Aryan Khurana <aryankhurana1511@gmail.com>
2022-11-08 11:08:52 -0500

    Added scroll functionality

--- 009-React.js/robofriends/src/App.js
+++ 009-React.js/robofriends/src/App.js
@@ -1,21 +1,25 @@
 import React from 'react'
-import {robots} from './robots'
 import CardList from './CardList'
 import SearchBox from './SearchBox'
+import Scroll from './Scroll'
 import './app.css';
 
-const state = {
-    robots: robots,
-    searchField: ''
-}
-
+// App is a smart component as it is a class and has a state
 class App extends React.Component {
     constructor() {
         super()
+        // State is a piece of data that describes our app
         this.state = {
-            robots: robots,
+            robots: [],
             searchField: ''
         }
+    }
+
+    // Function which runs code when the react component is mounted
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response => response.json())
+            .then(users => this.setState({robots: users}))
     }
 
     onSearchChange = (event) => {
@@ -26,14 +30,21 @@ class App extends React.Component {
         const filteredRobots = this.state.robots.filter(robot => {
             return robot.name.toLowerCase().includes(this.state.searchField.toLowerCase());
         })
-        return (
-            <div className='tc'>
-                <h1 className='f1'>RoboFriends</h1>
-                <SearchBox searchChange={this.onSearchChange}/>
-                <CardList robots={filteredRobots} />
-            </div>
-        )
-    }
+
+        if (this.state.robots.length === 0) {
+            return <h1>Loading</h1>;
+        } else {
+            return (
+                <div className='tc'>
+                    <h1 className='f1'>RoboFriends</h1>
+                    <SearchBox searchChange={this.onSearchChange}/>
+                    <Scroll>
+                        <CardList robots={filteredRobots} />
+                    </Scroll>
+                </div>
+            )
+        }
+}
 }
 
 export default App;
---
+++ 009-React.js/robofriends/src/Scroll.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Scroll = (props) => {
+    return (
+        <div style={{overflowY: 'scroll', border: '1px solid black', height: '800px'}}>
+            {props.children}
+        </div>
+    )
+};
+
+export default Scroll;
