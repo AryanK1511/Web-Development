32a63e21aff7e5e04504a6336aa2c5f2e1a4639e
Aryan Khurana <aryankhurana1511@gmail.com>
2022-11-08 10:37:41 -0500

    Added some styling

--- 009-React.js/robofriends/src/App.js
+++ 009-React.js/robofriends/src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import {robots} from './robots'
 import CardList from './CardList'
 import SearchBox from './SearchBox'
+import './app.css';
 
 const state = {
     robots: robots,
@@ -27,7 +28,7 @@ class App extends React.Component {
         })
         return (
             <div className='tc'>
-                <h1>RoboFriends</h1>
+                <h1 className='f1'>RoboFriends</h1>
                 <SearchBox searchChange={this.onSearchChange}/>
                 <CardList robots={filteredRobots} />
             </div>
