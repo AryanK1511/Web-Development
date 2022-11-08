5440721bda3c393d3d2bd8256a79313d06310392
Aryan Khurana <aryankhurana1511@gmail.com>
2022-11-08 11:17:59 -0500

    Made changes to the code

---
+++ 009-React.js/robofriends/src/containers/App.js
@@ -0,0 +1,49 @@
+import React from "react";
+import CardList from "../components/CardList";
+import SearchBox from "../components/SearchBox";
+import Scroll from "../components/Scroll";
+import "./app.css";
+
+// App is a smart component as it is a class and has a state
+class App extends React.Component {
+  constructor() {
+    super();
+    // State is a piece of data that describes our app
+    this.state = {
+      robots: [],
+      searchField: "",
+    };
+  }
+
+  // Function which runs code when the react component is mounted
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => this.setState({ robots: users }));
+  }
+
+  onSearchChange = (event) => {
+    this.setState({ searchField: event.target.value });
+  };
+
+  render() {
+    const { robots, searchField } = this.state;
+    const filteredRobots = robots.filter((robot) => {
+      return robot.name.toLowerCase().includes(searchField.toLowerCase());
+    });
+
+    return !robots.length ? (
+      <h1>Loading</h1>
+    ) : (
+      <div className="tc">
+        <h1 className="f1">RoboFriends</h1>
+        <SearchBox searchChange={this.onSearchChange} />
+        <Scroll>
+          <CardList robots={filteredRobots} />
+        </Scroll>
+      </div>
+    );
+  }
+}
+
+export default App;
