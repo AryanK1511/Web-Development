0f23b51fd68ddb48a14846c28bb99fea572411e9
Aryan Khurana <aryankhurana1511@gmail.com>
2022-11-08 18:42:53 -0500

    Completed emojipedia app

---
+++ 009-React.js/emojipedia/src/containers/App.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import Entry from '../components/Entry'
+import emojipedia from '../emojipedia'
+
+function createEntry(emojiTerm) {
+  return (
+    <Entry 
+      key={emojiTerm.id}
+      emoji={emojiTerm.emoji}
+      name={emojiTerm.name}
+      description={emojiTerm.meaning}
+    />
+  )
+}
+
+function App() {
+  return (
+    <div>
+      <h1>
+        <span>emojipedia</span>
+      </h1>
+
+      <dl className="dictionary">
+        {emojipedia.map(createEntry)}
+      </dl>
+    </div>
+  );
+}
+
+export default App;
--- 009-React.js/emojipedia/src/emojipedia.js
+++ 009-React.js/emojipedia/src/emojipedia.js
@@ -21,4 +21,5 @@ const emojipedia = [
         "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“."
     }
   ];
-  
+
+  export default emojipedia;
