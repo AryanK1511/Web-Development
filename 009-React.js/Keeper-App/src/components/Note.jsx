ac23289d6c03e6ef360373f1234b04f4d84870e4
Aryan Khurana <aryankhurana1511@gmail.com>
2022-11-08 20:25:52 -0500

    Configured Keeper app according to the ES6 syntax

--- 009-React.js/Keeper-App/src/components/Note.jsx
+++ 009-React.js/Keeper-App/src/components/Note.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 
-const Note = () => {
+const Note = (props) => {
     return(
         <div className="note">
-            <h1>This is the title</h1>
-            <p>This is the content</p>
+            <h1>{props.title}</h1>
+            <p>{props.content}</p>
         </div>
     );
 }
--- 009-React.js/Keeper-App/src/containers/App.jsx
+++ 009-React.js/Keeper-App/src/containers/App.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 import Header from '../components/Header';
 import Note from '../components/Note';
 import Footer from '../components/Footer';
+import notes from '../notes';
 
 class App extends React.Component {
     render() {
         return(
             <div>
                 <Header />
-                <Note />
+                {notes.map(noteItem => <Note key={noteItem.id} title={noteItem.title} content={noteItem.content}/>)};
                 <Footer />
             </div>
         )
