9fc07e6e924099063e6e8eab965913d5e1c24be3
Aryan Khurana <aryankhurana1511@gmail.com>
2022-11-12 14:15:37 -0500

    Completed dynamic form

--- 009-React.js/login-form/src/containers/App.js
+++ 009-React.js/login-form/src/containers/App.js
@@ -1,10 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [name, setName] = useState("");
+
+  function handleChange(event) {
+    setName(event.target.value);
+  }
+
   return (
     <div className="container">
-      <h1>Hello </h1>
-      <input type="text" placeholder="What's your name?" />
+      <h1>Hello {name}</h1>
+      <input onChange={handleChange} type="text" placeholder="What's your name?" />
       <button>Submit</button>
     </div>
   );
