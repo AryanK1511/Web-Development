d51792621375dc4a7bd9b5ce31ed6bd2245a72de
Aryan Khurana <aryankhurana1511@gmail.com>
2022-11-12 15:34:44 -0500

    Created login page

--- 009-React.js/login-form/src/containers/App.js
+++ 009-React.js/login-form/src/containers/App.js
@@ -1,17 +1,39 @@
 import React, { useState } from "react";
 
 function App() {
-  const [name, setName] = useState("");
+
+  const [fullName, setFullName] = useState({
+    fName: "",
+    lName: ""
+  });
 
   function handleChange(event) {
-    setName(event.target.value);
+    const { value, name } = event.target;
+    
+    setFullName(prevValue => {
+      if (name === "fName") {
+        return {
+          fName: value,
+          lName: prevValue.lName
+        }
+      } else if (name === "lName") {
+        return {
+          fName: prevValue.fName,
+          lName: value
+        }
+      }
+    })
+
   }
 
   return (
     <div className="container">
-      <h1>Hello {name}</h1>
-      <input onChange={handleChange} type="text" placeholder="What's your name?" />
-      <button>Submit</button>
+      <h1>Hello {fullName.fName} {fullName.lName}</h1>
+      <form>
+        <input name="fName" onChange={handleChange} placeholder="First Name" value={fullName.fName} />
+        <input name="lName" onChange={handleChange}  placeholder="Last Name" value={fullName.lName} />
+        <button>Submit</button>
+      </form>
     </div>
   );
 }
