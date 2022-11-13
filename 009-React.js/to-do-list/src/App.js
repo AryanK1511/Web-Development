ec7fd56c175cd3d4e0c75a091fd8288574cbdac2
Aryan Khurana <aryankhurana1511@gmail.com>
2022-11-12 22:02:29 -0500

    Completed to do list without component tree

--- 009-React.js/to-do-list/src/App.js
+++ 009-React.js/to-do-list/src/App.js
@@ -1,20 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [inputText, setInputText] = useState("");
+  const [items, setItems] = useState([]);
+
+  function handleChange(event) {
+    const newValue = event.target.value;
+    setInputText(newValue);
+  }
+
+  function addItem() {
+    setItems((prevItems) => {
+      return [...prevItems, inputText];
+    });
+    setInputText("");
+  }
+
   return (
     <div className="container">
       <div className="heading">
         <h1>To-Do List</h1>
       </div>
       <div className="form">
-        <input type="text" />
-        <button>
+        <input onChange={handleChange} type="text" value={inputText} />
+        <button onClick={addItem}>
           <span>Add</span>
         </button>
       </div>
       <div>
         <ul>
-          <li>A Item</li>
+        {
+          items.map((item) => {
+            return <li>{item}</li>
+          })
+        }
         </ul>
       </div>
     </div>
