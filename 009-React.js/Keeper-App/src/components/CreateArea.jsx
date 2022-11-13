a25f3c3e71cf1a2548a008b8385600e58ed1ae97
Aryan Khurana <aryankhurana1511@gmail.com>
2022-11-13 10:25:56 -0500

    Revert "Added classes and styles to prepare app for styling"
    This reverts commit bf8e28eb3ce71610fee1588ae40a25192868d8a0.

--- 009-React.js/Keeper-App/src/components/CreateArea.jsx
+++ 009-React.js/Keeper-App/src/components/CreateArea.jsx
@@ -1,47 +1,40 @@
 import React, { useState } from "react";
 
 function CreateArea(props) {
+
   const [note, setNote] = useState({
     title: "",
     content: ""
-  });
+  })
 
   function handleChange(event) {
-    const { name, value } = event.target;
+    const {name, value} = event.target;
 
     setNote(prevNote => {
       return {
+        // Spread operator spreads all of the key value pairs in the new object
         ...prevNote,
+        // The [] changes the name from a normal string to something with meaning
         [name]: value
-      };
-    });
+      }
+    })
   }
 
   function submitNote(event) {
     props.onAdd(note);
+    // Prevents the reloading of the page
+    event.preventDefault();
     setNote({
       title: "",
       content: ""
     });
-    event.preventDefault();
   }
 
   return (
     <div>
-      <form className="create-note">
-        <input
-          name="title"
-          onChange={handleChange}
-          value={note.title}
-          placeholder="Title"
-        />
-        <textarea
-          name="content"
-          onChange={handleChange}
-          value={note.content}
-          placeholder="Take a note..."
-          rows="3"
-        />
+      <form>
+        <input name="title" value={note.title} placeholder="Title" onChange={handleChange} />
+        <textarea name="content" value={note.content} placeholder="Take a note..." rows="3" onChange={handleChange} />
         <button onClick={submitNote}>Add</button>
       </form>
     </div>
--- 009-React.js/Keeper-App/src/containers/App.jsx
+++ 009-React.js/Keeper-App/src/containers/App.jsx
@@ -7,35 +7,32 @@ import CreateArea from "../components/CreateArea";
 function App() {
   const [notes, setNotes] = useState([]);
 
+  // Function which creates a notes - Receives note from CreateArea
   function addNote(newNote) {
     setNotes(prevNotes => {
-      return [...prevNotes, newNote];
-    });
+      return (
+        [...prevNotes, newNote]
+      );
+    })
   }
 
   function deleteNote(id) {
     setNotes(prevNotes => {
       return prevNotes.filter((noteItem, index) => {
         return index !== id;
-      });
-    });
+      })
+    })
   }
 
   return (
     <div>
       <Header />
       <CreateArea onAdd={addNote} />
-      {notes.map((noteItem, index) => {
-        return (
-          <Note
-            key={index}
-            id={index}
-            title={noteItem.title}
-            content={noteItem.content}
-            onDelete={deleteNote}
-          />
-        );
-      })}
+      {
+        notes.map((note, index) => {
+          return <Note key={index} id={index} title={note.title} content={note.content} onDelete={deleteNote}/>
+        })
+      }
       <Footer />
     </div>
   );
