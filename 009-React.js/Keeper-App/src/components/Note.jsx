bf8e28eb3ce71610fee1588ae40a25192868d8a0
Aryan Khurana <aryankhurana1511@gmail.com>
2022-11-13 09:52:03 -0500

    Added classes and styles to prepare app for styling

--- 009-React.js/Keeper-App/src/components/Note.jsx
+++ 009-React.js/Keeper-App/src/components/Note.jsx
@@ -1,18 +1,17 @@
 import React from "react";
 
-const Note = (props) => {
+function Note(props) {
+  function handleClick() {
+    props.onDelete(props.id);
+  }
 
-    function handleClick() {
-        props.onDelete(props.id);
-    }
-
-    return(
-        <div className="note">
-            <h1>{props.title}</h1>
-            <p>{props.content}</p>
-            <button onClick={handleClick}>DELETE</button>
-        </div>
-    );
+  return (
+    <div className="note">
+      <h1>{props.title}</h1>
+      <p>{props.content}</p>
+      <button onClick={handleClick}>DELETE</button>
+    </div>
+  );
 }
 
 export default Note;
