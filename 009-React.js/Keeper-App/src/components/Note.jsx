790c363c777069d790044f9bbdec9c91a841fed1
Aryan Khurana <aryankhurana1511@gmail.com>
2022-11-13 09:42:50 -0500

    Added add note functionality to the keeper app

--- 009-React.js/Keeper-App/src/components/Note.jsx
+++ 009-React.js/Keeper-App/src/components/Note.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 
 const Note = (props) => {
+
+    function handleClick() {
+        
+    }
+
     return(
         <div className="note">
             <h1>{props.title}</h1>
             <p>{props.content}</p>
+            <button onClick={handleClick}>DELETE</button>
         </div>
     );
 }
--- 009-React.js/Keeper-App/src/containers/App.jsx
+++ 009-React.js/Keeper-App/src/containers/App.jsx
@@ -1,15 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import Note from "../components/Note";
 import CreateArea from "../components/CreateArea";
 
 function App() {
+  const [notes, setNotes] = useState([]);
+
+  // Function which creates a notes - Receives note from CreateArea
+  function addNote(newNote) {
+    setNotes(prevNotes => {
+      return (
+        [...prevNotes, newNote]
+      );
+    })
+  }
+
   return (
     <div>
       <Header />
-      <CreateArea />
-      <Note key={1} title="Note title" content="Note content" />
+      <CreateArea onAdd={addNote} />
+      {
+        notes.map((note, index) => {
+          return <Note key={index} id={index} title={note.title} content={note.content}/>
+        })
+      }
       <Footer />
     </div>
   );
